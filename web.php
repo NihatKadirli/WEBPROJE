941bde06dcf46ba3d0d07b695d616e455bd2d690
NihatKadirli <nihadqedirli2@gmail.com>
2023-05-09 17:30:12 +0300

    loginde kullancı adı ve şifre için bir php sayfası oluşturdum

---
+++ web.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+<title>login</title>
+</head>
+<body>
+
+
+<div id="main">
+    <h1>Basit bir login</h1>
+    <form method="POST">
+        Kullanıcı Adı <input type="text" name="kullanıcı adı" class="text" required>
+        Şifre <input type="text">
+    </form>
+
+</div>
+    
+</body>
+</html>
